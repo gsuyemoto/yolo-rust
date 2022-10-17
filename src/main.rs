a4a37cd782c8ff800586bb43c7ebf6fc3a51afb4
gsuyemoto <gsuyemoto@gmail.com>
2022-10-17 13:01:47 -0700

    Stripped webcam and torch to bare min. Webcam saves single frame grab to file and torch reads single file to conduct inference. Using mobilenet-v3 model is the only model that works. Only other model that loads (TorchScript) keeps erroring out even with this simple example. The aforementioned model was created using script from YOLOv5 github repo.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,23 @@
 
 use anyhow::{bail, Result};
-use std::{time, path::Path, io::Read};
-
-//use opencv::{
-//	highgui,
-//	prelude::*,
-//	imgproc,
-//	videoio, 
-//    core,
-//};
 
 use linuxvideo::{
     format::{PixFormat, Pixelformat},
     Device,
 };
 
-const MODEL: &str           = "models/yolov5s.torchscript";
+use std::{env, fs::File, io::Write, path::Path};
+
+// const MODEL: &str           = "models/yolov5s.torchscript";
+const MODEL: &str           = "models/mobilenet-v3.pt";
 const WIDTH: i32            = 640;
 const HEIGHT: i32           = 480;
 const WINDOW_NAME: &str     = "YOLO Object Detection";
 
 pub fn main() -> Result<()>{
-    // create empty window named 'frame'
-    //highgui::named_window(WINDOW_NAME, highgui::WINDOW_NORMAL)?;
-    //highgui::resize_window(WINDOW_NAME, 640, 480)?;
-
     // load jit model and put it to cuda
     let mut model = tch::CModule::load_on_device(MODEL, tch::Device::Cpu)?;   
     model.set_eval(); 
-    // model.to(tch::Device::Cpu, tch::Kind::Float, false);
-
-    // create empty Mat to store image data
-    //let mut frame = Mat::default();
-
-    //// Open the web-camera (assuming you have one)
-    //let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
 
     let device = Device::open(Path::new("/dev/video0"))
         .expect("Unable to open camera device");
@@ -49,50 +32,23 @@ pub fn main() -> Result<()>{
     
     println!("stream started, waiting for data");
 
+    let path = Path::new("test.jpg");
+    let mut file = File::create(path)
+        .expect("Error creating file to save .jpg capture");
+
+    stream.dequeue(|buf| {
+        file.write_all(&*buf)?;
+        println!("wrote file");
+        Ok(())
+    })
+    .expect("Error, failed to write cam cap to file");
+
     loop {
-        // read frame to empty mat 
-        //cam.read(&mut frame)?;
-        
-        // resize image
-        //let mut resized = Mat::default();   
-        ////imgproc::resize(&frame, &mut resized, core::Size{width: WIDTH, height: HEIGHT}, 0.0, 0.0, opencv::imgproc::INTER_LINEAR)?;
-        //// convert bgr image to rgb
-        //let mut rgb_resized = Mat::default();  
-        ////imgproc::cvt_color(&resized, &mut rgb_resized, imgproc::COLOR_BGR2RGB, 0)?;    
-        //imgproc::cvt_color(&frame, &mut rgb_resized, imgproc::COLOR_BGR2RGB, 0)?;    
-        //// get data from Mat 
-        //let h = resized.size()?.height;
-        //let w = resized.size()?.width;   
-        //let resized_data = frame.data_bytes_mut()?; 
-        //// convert bytes to tensor
-        //let tensor = tch::Tensor::of_data_size(resized_data, &[HEIGHT as i64, WIDTH as i64, 3], tch::Kind::Uint8);  
-        //// normalize image tensor
-        //let tensor = tensor.to_kind(tch::Kind::Float) / 255;
-        //// carry tensor to cuda
-        //// let tensor = tensor.to_device(tch::Device::Cuda(0)); 
-        //let tensor = tensor.to_device(tch::Device::Cpu); 
-        //// convert (H, W, C) to (C, H, W)
-        //let tensor = tensor.permute(&[2, 0, 1]); 
-        //// add batch dim (convert (C, H, W) to (N, C, H, W)) 
-        //let normalized_tensor = tensor.unsqueeze(0);   
+        let tensor = tch::vision::imagenet::load_image(path)
+            .expect("Error loading video capture as Tensor");
+        let probabilities = model.forward_ts(&[tensor.unsqueeze(0)])?.softmax(-1, tch::Kind::Float);  
 
-        stream.dequeue(|buf| {
-            let tensor = tch::vision::imagenet::load_image_from_memory(&*buf)
-                .expect("Error loading video capture as Tensor");
-            let probabilities = model.forward_ts(&[tensor.unsqueeze(0)])?.softmax(-1, tch::Kind::Float);  
-
-            Ok(())
-        })
-        .expect("Error dequeueing stream");
-
-        //// show image 
-        //highgui::imshow(WINDOW_NAME, &frame)?;
-        //// if button q pressed, abort.
-        //if highgui::wait_key(5)? == 113 { 
-        //    highgui::destroy_all_windows()?;
-        //    println!("Pressed q. Aborting program.");
-        //    break;
-        //}
+        println!("{:?}", probabilities);
     }
   
     Ok(())
